a3955e6607791ce37fff0ba70ce4fae509f7dd51
Adam <adamware99@hotmail.com>
2021-07-18 23:18:49 -0400

    Updated cors policy

--- app.js
+++ app.js
@@ -75,7 +75,7 @@ app.use(session(sessionData));
 app.use(flash());
 
 app.use(function(req, res, next) {
-	res.header('Access-Control-Allow-Origin', 'https://webrevived.com');
+	res.header('Access-Control-Allow-Origin', '*');
 	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
 	next();
 });
