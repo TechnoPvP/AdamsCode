10cbbe7fbbe26486eb1f8c3732a47db89a6b07c3
Adam <adamware99@hotmail.com>
2021-07-25 20:06:42 -0400

    Increased file size

--- utils/UploadUtils.js
+++ utils/UploadUtils.js
@@ -22,7 +22,7 @@ const fileFilter = (req, file, cb) => {
 const upload = multer({
 	storage,
 	limits     : {
-		fileSize : 1024 * 1024 * 30
+		fileSize : 1024 * 1024 * 100
 	},
 	fileFilter : fileFilter
 });
